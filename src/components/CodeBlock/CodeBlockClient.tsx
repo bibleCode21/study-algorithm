668b4d1333a4b8c4564b7c39f59f6c9950ea4787
JokerJ <kjh5835@gmail.com>
2025-12-14 17:53:23 +0900

    perf: 코드 블럭에서 번호 나오도록 수정

--- src/components/CodeBlock/CodeBlockClient.tsx
+++ src/components/CodeBlock/CodeBlockClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 interface CodeBlockClientProps {
     language: string;
@@ -27,6 +27,27 @@ export default function CodeBlockClient({
         }
     };
 
+    // 줄 번호가 포함된 HTML 생성
+    const htmlWithLineNumbers = useMemo(() => {
+        const lines = code.split('\n');
+        const totalLines = lines.length;
+        const lineNumberWidth = totalLines.toString().length;
+
+        // shiki가 생성하는 HTML 구조를 파싱하여 각 줄에 줄 번호 추가
+        // <span class="line"> 태그를 찾아서 줄 번호 추가
+        let lineIndex = 0;
+        const processedHtml = highlightedHtml.replace(
+            /<span class="line">/g,
+            () => {
+                lineIndex++;
+                const paddedLineNumber = lineIndex.toString().padStart(lineNumberWidth, ' ');
+                return `<span class="line flex"><span class="text-gray-600 select-none text-right inline-block shrink-0" style="min-width: ${lineNumberWidth * 0.6 + 0.5}em; margin-right: 1rem; user-select: none">${paddedLineNumber}</span><span class="flex-1">`;
+            }
+        );
+
+        return processedHtml;
+    }, [code, highlightedHtml]);
+
     return (
         <div className={`bg-gray-900 rounded-lg overflow-hidden ${className}`}>
             <div className="flex items-center justify-between px-4 py-2 bg-gray-800 border-b border-gray-700">
@@ -40,8 +61,8 @@ export default function CodeBlockClient({
                 </button>
             </div>
             <div
-                className="p-4 overflow-x-auto [&_pre]:!bg-transparent [&_pre]:!p-0 [&_pre]:!m-0 [&_code]:!text-sm [&_code]:!font-mono [&_code]:!leading-relaxed"
-                dangerouslySetInnerHTML={{ __html: highlightedHtml }}
+                className="p-4 overflow-x-auto [&_pre]:!bg-transparent [&_pre]:!p-0 [&_pre]:!m-0 [&_code]:!text-sm [&_code]:!font-mono [&_code]:!leading-relaxed [&_code]:!block [&_.line]:!block"
+                dangerouslySetInnerHTML={{ __html: htmlWithLineNumbers }}
             />
         </div>
     );
