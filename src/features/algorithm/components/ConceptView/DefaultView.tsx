13e746d2a23851fb35a7837a4c08d253da5b18cb
JokerJ <kjh5835@gmail.com>
2026-01-07 03:04:55 +0900

    perf: 컴팩트와 중복되는 부분 삭제

--- src/features/algorithm/components/ConceptView/DefaultView.tsx
+++ src/features/algorithm/components/ConceptView/DefaultView.tsx
@@ -1,6 +1,5 @@
 import { ConceptViewProps } from '@/features/algorithm/types/components';
 import ConceptDescription from '@/features/algorithm/components/ConceptDetail/ConceptDescription';
-import ConceptComplexity from '@/features/algorithm/components/ConceptDetail/ConceptComplexity';
 
 type DefaultViewProps = Omit<ConceptViewProps, 'viewMode'>;
 
@@ -8,12 +7,6 @@ const DefaultView = ({ concept }: DefaultViewProps) => {
     return (
         <>
             <ConceptDescription description={concept.description} />
-            {concept.type === 'algorithm' && concept.timeComplexity && (
-                <ConceptComplexity
-                    timeComplexity={concept.timeComplexity}
-                    spaceComplexity={concept.spaceComplexity}
-                />
-            )}
         </>
     );
 };
